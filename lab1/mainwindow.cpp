93d107b27e2394e8621e397ca6db9242b321831b
SandraChen <819562396@qq.com>
2024-10-12 00:01:36 +0800

    第一次提交

--- lab1/mainwindow.cpp
+++ lab1/mainwindow.cpp
@@ -6,10 +6,101 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    connect(ui->btnNum0,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    connect(ui->btnNum1,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    connect(ui->btnNum2,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    connect(ui->btnNum3,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    connect(ui->btnNum4,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    connect(ui->btnNum5,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    connect(ui->btnNum6,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    connect(ui->btnNum7,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    connect(ui->btnNum8,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    connect(ui->btnNum9,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+
+    connect(ui->btnPlus,SIGNAL(clicked()),this,SLOT(btnBinaryOperatorClicked()));
+    connect(ui->btnMinus,SIGNAL(clicked()),this,SLOT(btnBinaryOperatorClicked()));
+    connect(ui->btnMultiple,SIGNAL(clicked()),this,SLOT(btnBinaryOperatorClicked()));
+    connect(ui->btnDivide,SIGNAL(clicked()),this,SLOT(btnBinaryOperatorClicked()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+QString MainWindow::calculation(bool *ok)
+{
+    if(operands.size()==2 && opcode.size()>0)
+    {
+        ui->statusbar->showMessage("calcuation is in progress");
+    }
+    else {
+        ui->statusbar->showMessage(QString("operands id {1} ,opcode is {2} ").arg(operands.size()).arg(opcode.size()));
+    }
+    return "";
+}
+
+void MainWindow::btnNumClicked()
+{
+    QString digit = qobject_cast<QPushButton*>(sender())->text();
+    if(digit =="0" && operand == "0")
+        digit ="";
+    if(operand == "0" && digit != "0")
+        operand = "";
+    operand += digit;
+    ui->display->setText(operand);
+    ui->statusbar->showMessage(qobject_cast<QPushButton*>(sender())->text() + "btn clicked");
+
+}
+
+void MainWindow::on_btnPeriod_clicked()
+{
+    if(!operand.contains("."))
+        operand += qobject_cast<QPushButton*>(sender())->text();
+    ui->display->setText(operand);
+}
+
+
+void MainWindow::on_btnDel_clicked()
+{
+    operand = operand.left(operand.length()-1);
+    ui->display->setText(operand);
+}
+
+
+void MainWindow::on_btnClearAll_clicked()
+{
+    operand.clear();
+    ui->display->setText(operand);
+
+}
+
+void MainWindow::btnBinaryOperatorClicked()
+{
+    ui->statusbar->showMessage("last operand" + operand);
+    QString opcode = qobject_cast<QPushButton*>(sender())->text();
+    if(operand != "")
+    {
+        operands.push_back(operand);
+        operand ="";
+
+        opcodes.push_back(opcode);
+    }
+
+    QString result = calculation();
+    ui->display->setText(result);
+
+}
+
+void MainWindow::on_btnEqual_clicked()
+{
+    if(operand != "")
+    {
+        operands.push_back(operand);
+        operand ="";
+    }
+    QString result = calculation();
+    ui->display->setText(result);
 }
 
